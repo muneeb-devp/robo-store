06f18f3794455baddbbfe772e536c9aae3176de5
Muneeb Mughal <muneeb.devp@gmail.com>
2024-06-02 11:22:18 +0500

    Ads site-header component

---
+++ src/app/site-header/site-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'robo-site-header',
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css'],
+  standalone: true,
+})
+export class SiteHeaderComponent {
+  constructor() {}
+}
